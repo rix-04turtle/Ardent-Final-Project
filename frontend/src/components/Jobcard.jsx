171fb2bec1cd548ccdf93e13bc4c1441c52192eb
rix-04turtle <rishita.k004@gmail.com>
2026-03-04 23:58:47 +0530

    some changes

--- frontend/src/components/Jobcard.jsx
+++ frontend/src/components/Jobcard.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import{
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  CardActions,
+  CardMedia,
+  Typography,
+  Stack,
+  Chip,
+  Button,
+  CardActions,
+} from '@mui/material'
+import {link} from 'react-router-dom';
+import { getimageurl } from '../utils/getimageurl';
 
-const Jobcard = () => {
+
+
+const Jobcard = ({job,onDelete}) => {
+  const img = job?.jobimage ? getimageurl(job.jobimage) : "";
   return (
     <div>
-      
+      <card>
+        {img ? (
+          <CardMedia
+            component="img"
+            height="140"
+            image={img}
+            alt={job?.jobtitle}
+          />
+        ) : (
+          <CardMedia> 
+            <Typography variant="h5" component="div" align="center">
+              No Image Available
+            </Typography>
+           </CardMedia>)}
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {job?.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {job?.description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Company: {job?.company}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Location: {job?.location}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Salary: {job?.salary}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Job Type: {job?.jobType}
+          </Typography>
+        </CardContent>
+      </card>
     </div>
   )
 }
--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Box, CircularProgress } from '@mui/material';
 
 const Loader = () => {
   return (
     <div>
-      
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+        <CircularProgress />
+      </Box>
+
     </div>
   )
 }
